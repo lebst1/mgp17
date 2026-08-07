37f5d95a728bfa0f66d8d29071d9fc32d6a859d8
lebst1 <hogor228339@mail.ru>
2026-08-08 02:56:47 +0300

    first commit

--- src/bot/handlers/__init__.py
+++ src/bot/handlers/__init__.py
@@ -1,15 +1,17 @@
 from .start import router as start_router
-from .save_mode import router as savemode_router
-from .admin import router as admin_router
-from .subscription import router as subscription_router
 from .profile import router as profile_router
 from .referral import router as referral_router
+from .subscription import router as subscription_router
+from .admin import router as admin_router
+from .save_mode import router as save_mode_router  # 👈 ДОБАВЛЯЕМ
+from .settings import router as settings_router
 
 __all__ = [
     "start_router",
-    "savemode_router",
-    "admin_router",
-    "subscription_router",
     "profile_router",
     "referral_router",
+    "subscription_router",
+    "admin_router",
+    "save_mode_router",  # 👈 ДОБАВЛЯЕМ
+    "settings_router",
 ]
